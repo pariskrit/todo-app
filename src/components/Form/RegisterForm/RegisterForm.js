abb5d01aa8c65d8bb40a34e82db733a911c90bde
Pariskrit Moktan <moktanpariskrit@gmail.com>
2020-10-28 14:39:50 +0545

    registerform validation complete

--- src/components/Form/RegisterForm/RegisterForm.js
+++ src/components/Form/RegisterForm/RegisterForm.js
@@ -4,7 +4,7 @@ import useForm from "../../../customhooks/useForm/useForm";
 import { Button } from "react-bootstrap";
 
 const RegisterForm = () => {
-  const { values, handleChange, error } = useForm({
+  const { values, handleChange, error, formValidate } = useForm({
     name: "",
     email: "",
     password: "",
@@ -12,6 +12,7 @@ const RegisterForm = () => {
 
   function register(e) {
     e.preventDefault();
+
     console.log(values);
   }
 
@@ -27,9 +28,9 @@ const RegisterForm = () => {
             name="name"
             value={values.name}
             onChange={handleChange}
-            className={`register__input ${error.name && "error"}`}
+            className={`register__input ${error.name.name && "error"}`}
           />
-          <p className="register__error">{error.name}</p>
+          <p className="register__error">{error.name.name}</p>
         </div>
 
         <div className="register__field">
@@ -40,9 +41,9 @@ const RegisterForm = () => {
             name="email"
             value={values.email}
             onChange={handleChange}
-            className={`register__input ${error.email && "error"}`}
+            className={`register__input ${error.email.email && "error"}`}
           />
-          <p className="register__error">{error.email}</p>
+          <p className="register__error">{error.email.email}</p>
         </div>
         <div className="register__field">
           <b>password:</b>
@@ -52,12 +53,19 @@ const RegisterForm = () => {
             name="password"
             value={values.password}
             onChange={handleChange}
-            className="register__input"
+            className={`register__input ${error.password.password && "error"}`}
           />
+          <p className="register__error">{error.password.password}</p>
         </div>
-        <Button variant="primary" type="submit">
-          Register
-        </Button>
+        {!formValidate ? (
+          <button type="button" className="register__buttondisable">
+            register
+          </button>
+        ) : (
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        )}
       </form>
     </div>
   );
--- src/components/Form/formValidation/formValidation.js
+++ src/components/Form/formValidation/formValidation.js
@@ -1,16 +1,61 @@
-import { useState } from "react";
-
-const formValidation = (values) => {
+export const nameValidation = (values) => {
   let error = {};
 
   if (!values.name) {
     error.name = "name is required";
-  }
-  if (!/\S+@\S+\.\S+/.test(values.email) && values.email) {
-    error.email = "email is Invalid";
+    error.validate = false;
+  } else {
+    error.name = "";
+    error.validate = true;
   }
 
   return error;
 };
 
-export default formValidation;
+export const emailValidation = (values) => {
+  let error = {};
+
+  if (!values.email) {
+    error.email = "email is required";
+    error.validate = false;
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    error.email = "email is Invalid";
+    error.validate = false;
+  } else {
+    error.email = "";
+    error.validate = true;
+  }
+
+  return error;
+};
+
+export const passwordValidation = (values) => {
+  let error = {};
+
+  if (!values.password) {
+    error.password = "password is required";
+    error.validate = false;
+  } else if (values.password.length < 8) {
+    error.password = "password must be greator than 8 letters";
+    error.validate = false;
+  } else {
+    error.password = "";
+    error.validate = true;
+  }
+
+  return error;
+};
+
+export const formValidate = (values) => {
+  let val = false;
+
+  if (
+    values.name.validate &&
+    values.email.validate &&
+    values.password.validate
+  ) {
+    val = true;
+  }
+
+  return val;
+};
